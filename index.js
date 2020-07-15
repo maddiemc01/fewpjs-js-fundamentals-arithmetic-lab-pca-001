2339efcc7b847a9db9cd7e1031f40c093fd4240f
Madison McCardle <madison.mccardle@powerhrg.com>
2020-07-15 12:07:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+
+const multiply = `${num1}` * `${num2}`
+
+const random = Math.floor(Math.random() * Infinity)
+
+const mod = 24 % 5
+
+const max = Math.max(1, 20, 13, 5)
